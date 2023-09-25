02b7f5af00aadd0b592b0b8c3c5179c17e0e67d9
fredrikmonsen <31658585+fredrikmonsen@users.noreply.github.com>
2023-09-25 13:59:13 +0200

    docs: add openapi swagger documentation to endpoints (#6)
    Adds the openapi dependency with swagger UI
    
    Internal ref: TT-1104

--- src/main/kotlin/no/nb/bikube/newspaper/controller/TitleController.kt
+++ src/main/kotlin/no/nb/bikube/newspaper/controller/TitleController.kt
@@ -1,19 +1,29 @@
 package no.nb.bikube.newspaper.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import no.nb.bikube.core.model.Title
 import no.nb.bikube.newspaper.service.AxiellService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/newspapers")
+@Tag(name="Newspaper titles", description="Endpoints related to newspaper titles.")
+@RequestMapping("/newspapers/titles")
 class TitleController (
     private val axiellService: AxiellService
 ) {
-    @GetMapping("/")
+    @GetMapping("/", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Operation(summary = "Get all newspaper titles")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "OK"),
+        ApiResponse(responseCode = "500", description = "Server error")
+    ])
     fun getTitles(): Flux<Title> {
         return axiellService.getTitles()
     }
