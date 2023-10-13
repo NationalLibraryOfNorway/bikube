72c0a173e216dbafe43fedc5db15b63f33b124cb
fredrikmonsen <fredrik.mons1@hotmail.com>
2023-10-13 15:40:05 +0200

    permit get requests for all

--- src/main/kotlin/no/nb/bikube/core/configuration/SecurityConfig.kt
+++ src/main/kotlin/no/nb/bikube/core/configuration/SecurityConfig.kt
@@ -3,6 +3,7 @@ package no.nb.bikube.core.configuration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -36,6 +37,7 @@ class SecurityConfig {
                             "/actuator",
                             "/actuator/**"
                         ).permitAll()
+                        .pathMatchers(HttpMethod.GET, "/**").permitAll()
                         .anyExchange().authenticated()
                 }
                 .httpBasic {  }
