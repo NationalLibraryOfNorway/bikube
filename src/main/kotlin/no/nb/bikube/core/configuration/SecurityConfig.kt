8647eeebfc6daefda767e4a81312568cd3ac3400
Fredrik Monsen <31658585+fredrikmonsen@users.noreply.github.com>
2023-10-16 07:35:35 +0200

    feat: Add and enable basic auth (TT-1164) (#32)
    Adds basic authentication with username/pw to application. Allows GET-requests for everyone. Secrets added to kubernetes and referenced in internal deployment yaml.

---
+++ src/main/kotlin/no/nb/bikube/core/configuration/SecurityConfig.kt
@@ -0,0 +1,68 @@
+package no.nb.bikube.core.configuration
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @ConditionalOnProperty(
+        prefix = "security",
+        name = ["enabled"],
+        havingValue = "true"
+    )
+    @Configuration
+    @EnableWebFluxSecurity
+    class Enabled {
+        @Bean
+        fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+            http
+                .csrf{ csrf -> csrf.disable()}
+                .authorizeExchange { exchange ->
+                    exchange
+                        .pathMatchers(
+                            "/",
+                            "/webjars/swagger-ui/**",
+                            "/v3/api-docs",
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html",
+                            "/swagger-resources",
+                            "/swagger-resources/**",
+                            "/actuator",
+                            "/actuator/**"
+                        ).permitAll()
+                        .pathMatchers(HttpMethod.GET, "/**").permitAll()
+                        .anyExchange().authenticated()
+                }
+                .httpBasic {  }
+            return http.build()
+        }
+    }
+
+    @ConditionalOnProperty(
+        prefix = "security",
+        name = ["enabled"],
+        havingValue = "false"
+    )
+    @Configuration
+    @EnableWebFluxSecurity
+    class Disabled {
+        @Bean
+        fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+            http
+                .csrf{ csrf -> csrf.disable()}
+                .authorizeExchange { authorizeExchange ->
+                    authorizeExchange
+                        .anyExchange()
+                        .permitAll()
+                }
+            return http.build()
+        }
+    }
+}
