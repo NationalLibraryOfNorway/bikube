c949857458cbd32039c433bd145a93b8fb7feb7b
fredrikmonsen <fredrik.mons1@hotmail.com>
2023-11-06 08:48:22 +0100

    add realm and kdc to properties

--- src/main/kotlin/no/nb/bikube/newspaper/config/KerberosConfig.kt
+++ src/main/kotlin/no/nb/bikube/newspaper/config/KerberosConfig.kt
@@ -1,12 +1,22 @@
 package no.nb.bikube.newspaper.config
 
 import jakarta.annotation.PostConstruct
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
+@ConfigurationProperties("kerberos")
+class KerberosConfigProperties (
+    var realm: String,
+    var kdc: String
+)
+
 @Configuration
-class KerberosConfig {
+class KerberosConfig(
+    private val krb5: KerberosConfigProperties
+) {
     @PostConstruct
     fun setKerberosConfigurationPath() {
-        System.setProperty("java.security.krb5.conf", "src/main/resources/krb5.conf")
+        System.setProperty("java.security.krb5.realm", krb5.realm)
+        System.setProperty("java.security.krb5.kdc", krb5.kdc)
     }
 }
