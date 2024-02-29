15c72a3fde025aa78740d04dcce53e16ec172e69
Marius Levang <marius.levang@nb.no>
2024-02-29 13:58:40 +0100

    feat: Change to keycloak auth (TT-1471)

--- src/main/kotlin/no/nb/bikube/core/configuration/SecurityConfig.kt
+++ src/main/kotlin/no/nb/bikube/core/configuration/SecurityConfig.kt
@@ -3,10 +3,18 @@ package no.nb.bikube.core.configuration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
 import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
 import org.springframework.security.web.server.SecurityWebFilterChain
+import reactor.core.publisher.Mono
 
 @Configuration
 class SecurityConfig {
@@ -22,7 +30,7 @@ class SecurityConfig {
         @Bean
         fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
             http
-                .csrf{ csrf -> csrf.disable()}
+                .csrf { csrf -> csrf.disable() }
                 .authorizeExchange { exchange ->
                     exchange
                         .pathMatchers(
@@ -38,10 +46,30 @@ class SecurityConfig {
                             "/actuator/**"
                         ).permitAll()
                         .pathMatchers(HttpMethod.GET, "/**").permitAll()
-                        .anyExchange().authenticated()
+                        .anyExchange().hasAuthority("bikube-create")
                 }
-                .httpBasic {  }
+                .oauth2ResourceServer { server ->
+                    server.jwt { jwt ->
+                        jwt.jwtAuthenticationConverter(authoritiesExtractor())
+                    }
+                }
             return http.build()
+        }
+
+        fun authoritiesExtractor(): Converter<Jwt, Mono<AbstractAuthenticationToken>> {
+            val jwtAuthConverter = JwtAuthenticationConverter()
+            jwtAuthConverter.setJwtGrantedAuthoritiesConverter(GrantedAuthoritiesExtractor())
+            return ReactiveJwtAuthenticationConverterAdapter(jwtAuthConverter)
+        }
+
+        internal class GrantedAuthoritiesExtractor : Converter<Jwt, Collection<GrantedAuthority>> {
+            override fun convert(jwt: Jwt): Collection<GrantedAuthority> {
+                @Suppress("UNCHECKED_CAST")
+                val realmAccess: Map<String, List<String>> = jwt.claims.getOrDefault("realm_access", emptyMap<String, List<String>>()) as Map<String, List<String>>
+                val roles: List<String> = realmAccess.getOrDefault("roles", emptyList())
+
+                return roles.map { SimpleGrantedAuthority(it) }
+            }
         }
     }
 
@@ -56,7 +84,7 @@ class SecurityConfig {
         @Bean
         fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
             http
-                .csrf{ csrf -> csrf.disable()}
+                .csrf { csrf -> csrf.disable() }
                 .authorizeExchange { authorizeExchange ->
                     authorizeExchange
                         .anyExchange()
