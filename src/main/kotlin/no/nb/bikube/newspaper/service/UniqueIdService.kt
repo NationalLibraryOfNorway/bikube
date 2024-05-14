78c46adc99e5110efb6032acd561debee5e0ec92
Fredrik Monsen <fredrik.monsen@nb.no>
2024-05-14 12:08:34 +0200

    Apply suggestions

--- src/main/kotlin/no/nb/bikube/newspaper/service/UniqueIdService.kt
+++ src/main/kotlin/no/nb/bikube/newspaper/service/UniqueIdService.kt
@@ -9,7 +9,7 @@ class UniqueIdService(
     private val entityManager: EntityManager
 ) {
     fun getUniqueId(): String {
-        val query = entityManager.createNativeQuery("SELECT nextval('collections_id_seq')")
+        val query = entityManager.createNativeQuery("SELECT nextval('maxit.collections_id_seq')")
         val id = query.singleResult.toString()
         logger().info("Got unique id $id from database sequence")
         return id
