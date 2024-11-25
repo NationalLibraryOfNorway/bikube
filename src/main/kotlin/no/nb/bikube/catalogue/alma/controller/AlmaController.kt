017348c6fedfbd2d1f038cf74e5b8b1b6a91cb9e
Fredrik Monsen <31658585+fredrikmonsen@users.noreply.github.com>
2024-11-25 13:51:34 +0100

    feat: add template engine support for compatibility with POS app (TT-1790) (#98)

--- src/main/kotlin/no/nb/bikube/catalogue/alma/controller/AlmaController.kt
+++ src/main/kotlin/no/nb/bikube/catalogue/alma/controller/AlmaController.kt
@@ -7,14 +7,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.constraints.Pattern
-import no.nb.bikube.catalogue.alma.service.*
+import no.nb.bikube.catalogue.alma.service.AlmaService
+import no.nb.bikube.catalogue.alma.service.AlmaSruService
+import no.nb.bikube.catalogue.alma.service.MarcXChangeService
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 
 @Validated
--- src/main/kotlin/no/nb/bikube/catalogue/alma/service/AlmaService.kt
+++ src/main/kotlin/no/nb/bikube/catalogue/alma/service/AlmaService.kt
@@ -10,8 +10,8 @@ import no.nb.bikube.catalogue.alma.model.MarcRecord
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
-import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @Service
