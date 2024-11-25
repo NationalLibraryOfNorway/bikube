c8cc181a3f7fcda7eb1455dfea57d6d45042a52a
Fredrik Monsen <fredrik.monsen@nb.no>
2024-11-25 13:14:00 +0100

    simplify

--- src/main/kotlin/no/nb/bikube/catalogue/alma/controller/AlmaHTMLController.kt
+++ src/main/kotlin/no/nb/bikube/catalogue/alma/controller/AlmaHTMLController.kt
@@ -2,11 +2,14 @@ package no.nb.bikube.catalogue.alma.controller
 
 import io.swagger.v3.oas.annotations.tags.Tag
 import no.nb.bikube.catalogue.alma.enum.OtherField
+import no.nb.bikube.catalogue.alma.model.MarcRecord
 import no.nb.bikube.catalogue.alma.service.AlmaService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import reactor.core.publisher.Mono
+import java.util.*
 
 /**
  * Controller for handling requests for bibliographic records in HTML format.
@@ -25,30 +28,27 @@ class AlmaHTMLController(
     fun getHTMLByBarcode(
         model: Model,
         @PathVariable barcode: String
-    ): String {
-        val metadata = almaService.getRecordByBarcode(barcode)
+    ): Mono<String> {
+        return almaService.getRecordByBarcode(barcode).map {
+            model.addAttribute("id", barcode)
+            OtherField.entries.forEach { field ->
+                mapMetadata(it, field)?.let { value ->
+                    model.addAttribute(field.name.lowercase(Locale.getDefault()), value)
+                }
+            }
+            "marc_record"
+        }.onErrorResume { e ->
+            model.addAttribute("error", e.message)
+            Mono.just("error")
+        }
+    }
 
-        metadata.mapNotNull { md -> md.datafield.find { df ->
-            df.tag == OtherField.TITLE.tag
-        }?.subfield?.find { sf -> sf.code == OtherField.TITLE.code }?.content }.let {
-            model.addAttribute("titleObject", it)
-        }
-        metadata.mapNotNull { md -> md.datafield.find { df ->
-            df.tag == OtherField.AUTHOR.tag
-        }?.subfield?.find { sf -> sf.code == OtherField.AUTHOR.code }?.content }.let {
-            model.addAttribute("authorObject", it)
-        }
-        metadata.mapNotNull { md -> md.datafield.find { df ->
-            df.tag == OtherField.YEAR.tag
-        }?.subfield?.find { sf -> sf.code == OtherField.YEAR.code }?.content }.let {
-            model.addAttribute("yearObject", it)
-        }
-        metadata.mapNotNull { md -> md.datafield.find { df ->
-            df.tag == OtherField.NUMBER_OF_PAGES.tag
-        }?.subfield?.find { sf -> sf.code == OtherField.NUMBER_OF_PAGES.code }?.content }.let {
-            model.addAttribute("numberOfPagesObject", it)
-        }
 
-        return "marc_record"
+    companion object {
+        fun mapMetadata(metadata: MarcRecord, otherField: OtherField): String? {
+            return metadata.datafield.find { df ->
+                df.tag == otherField.tag
+            }?.subfield?.find { sf -> sf.code == otherField.code }?.content
+        }
     }
 }
--- src/main/kotlin/no/nb/bikube/catalogue/alma/service/AlmaService.kt
+++ src/main/kotlin/no/nb/bikube/catalogue/alma/service/AlmaService.kt
@@ -87,27 +87,6 @@ class AlmaService(
             }
     }
 
-//    @Throws(NotFoundException::class, AlmaException::class, MarcXChangeException::class)
-//    fun getMarcXChangeRecordByBarcode(barcode: String): MarcXChangeRecord {
-//        val mms: String
-//        val enumChron: Map<EnumChronField, String>
-//        try {
-//            val result = almaClientService.getItem(barcode)
-//            if (result.getMMS().isEmpty()) {
-//                throw UncategorizedException("No MMS-id found in Alma item result.")
-//            } else mms = result.getMMS()
-//            enumChron = result.getEnumChron()
-//        } catch (ex: ErrorResponseException) {
-//            if (ex.containsErrorCode(AlmaErrorCode.BARCODE_NOT_FOUND.value)) {
-//                throw NotFoundException("Barcode not found", ex)
-//            }
-//            throw ex
-//        }
-//        val marcXChangeRecord = getRecordByMMS(mms)
-//        marcXChangeRecord.addEnumChron(enumChron)
-//        return marcXChangeRecord
-//    }
-
     private fun mapHttpError(response: ClientResponse): Mono<Throwable> {
         if (response.statusCode() != HttpStatus.BAD_REQUEST)
             return response.bodyToMono(String::class.java)
