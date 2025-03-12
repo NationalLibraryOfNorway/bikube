a4d3baeb051886f2aae8017824014fc679336374
Pierre Beauguitte <pierre.beauguitte@nb.no>
2025-03-12 12:28:18 +0100

    Try w7o static companion

--- src/test/kotlin/no/nb/bikube/catalogue/alma/service/AlmaSruServiceTests.kt
+++ src/test/kotlin/no/nb/bikube/catalogue/alma/service/AlmaSruServiceTests.kt
@@ -7,9 +7,7 @@ import no.nb.bikube.catalogue.alma.exception.AlmaRecordNotFoundException
 import no.nb.bikube.core.util.logger
 import org.hamcrest.MatcherAssert
 import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,16 +15,13 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.util.StreamUtils
 import org.xmlunit.matchers.CompareMatcher
 import reactor.test.StepVerifier
-import java.net.ServerSocket
-import kotlin.properties.Delegates
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AlmaSruServiceTests(
     @Autowired private val almaSruService: AlmaSruService,
     @Autowired private val marcXChangeService: MarcXChangeService
@@ -34,7 +29,7 @@ class AlmaSruServiceTests(
 
     lateinit var mockBackEnd: WireMockServer
 
-    @BeforeEach
+    @BeforeAll
     fun setup() {
         logger().info("Setting up WireMockServer...")
         mockBackEnd = WireMockServer(WireMockConfiguration.options().port(12345))
@@ -45,44 +40,16 @@ class AlmaSruServiceTests(
         logger().info("Done!")
     }
 
-    @AfterEach
+    @AfterAll
     fun shutdown() {
+        logger().info("Shutting down")
         mockBackEnd.shutdown()
+        logger().info("Finished")
     }
-
-//    companion object {
-//
-//        @JvmStatic
-//        lateinit var mockBackEnd: WireMockServer
-//
-//        @JvmStatic
-//        var port by Delegates.notNull<Int>()
-//
-//        @JvmStatic
-//        @BeforeAll
-//        fun setup() {
-//            mockBackEnd = WireMockServer(WireMockConfiguration.options().port(port))
-//            mockBackEnd.start()
-//            configureFor("localhost", port)
-//        }
-//
-//        @JvmStatic
-//        @DynamicPropertySource
-//        fun properties(r: DynamicPropertyRegistry) {
-//            val serverSocket = ServerSocket(0)
-//            port = serverSocket.localPort
-//            r.add("alma.alma-sru-url") { "http://localhost:$port" }
-//        }
-//
-//        @JvmStatic
-//        @AfterAll
-//        fun shutdown() {
-//            mockBackEnd.shutdown()
-//        }
-//    }
 
     @Test
     fun `Alma SRU response should be mapped to correct RecordList`() {
+        logger().info("Running test 1")
         val sruResponse = StreamUtils.copyToString(
             ClassPathResource("AlmaXmlTestFiles/sru_result.xml").inputStream,
             Charsets.UTF_8
@@ -91,7 +58,7 @@ class AlmaSruServiceTests(
             ClassPathResource("AlmaXmlTestFiles/marc_issn.xml").inputStream
         )
 
-        stubFor(any(anyUrl())
+        stubFor(get(urlPathMatching("/47BIBSYS_NB"))
             .willReturn(
                 aResponse()
                     .withStatus(HttpStatus.OK.value())
@@ -109,12 +76,13 @@ class AlmaSruServiceTests(
 
     @Test
     fun `An empty SRU response should be mapped to AlmaRecordNotFoundException`() {
+        logger().info("Running test 2")
         val sruResponse = StreamUtils.copyToString(
             ClassPathResource("AlmaXmlTestFiles/sru_result_empty.xml").inputStream,
             Charsets.UTF_8
         )
 
-        stubFor(any(anyUrl())
+        stubFor(get(urlPathMatching("/47BIBSYS_NB"))
             .willReturn(
                 aResponse()
                     .withStatus(HttpStatus.OK.value())
