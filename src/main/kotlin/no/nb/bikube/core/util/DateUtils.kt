841bb6ef85d31998cd196b0441567a192f939432
Sindre Østrem <sindre.ostrem@nb.no>
2025-08-05 13:54:45 +0200

    fix parse dates with format YYYY-MM

--- src/main/kotlin/no/nb/bikube/core/util/DateUtils.kt
+++ src/main/kotlin/no/nb/bikube/core/util/DateUtils.kt
@@ -4,6 +4,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 val yearRegex = Regex("^\\d{4}$")
+val dateWithDashShortRegex = Regex("^\\d{4}-\\d{2}$")
 val dateWithDashRegex = Regex("^\\d{4}-\\d{2}-\\d{2}$")
 val dateWithDotsRegex = Regex("^\\d{4}\\.\\d{2}\\.\\d{2}$")
 
@@ -14,6 +15,8 @@ class DateUtils {
                 null
             } else if (date.matches(yearRegex)) {
                 LocalDate.parse("$date-01-01")
+            }  else if (date.matches(dateWithDashShortRegex)) {
+                LocalDate.parse("$date-01")
             } else if (date.matches(dateWithDashRegex)) {
                 LocalDate.parse(date)
             } else if (date.matches(dateWithDotsRegex)){
