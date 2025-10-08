0d6cab62fb3812d579230c9bbc01158a27d7e729
Gard Marius Guldhav <gard.guldhav@nb.no>
2025-10-08 15:25:53 +0200

    Feat/Add search and navigation to item

--- src/main/frontend/components/header.tsx
+++ src/main/frontend/components/header.tsx
@@ -1,54 +1,59 @@
-import { NavLink } from "react-router";
-import { Button, HorizontalLayout } from "@vaadin/react-components";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { LogOut, Sun, Moon } from "lucide-react";
+import {NavLink, useLocation} from "react-router";
+import {Button, HorizontalLayout} from "@vaadin/react-components";
+import {SidebarTrigger} from "@/components/ui/sidebar";
+import {Avatar, AvatarFallback} from "@/components/ui/avatar";
+import {LogOut, Sun, Moon} from "lucide-react";
 import Logo from "@/components/logo";
-import { translate, key } from "@vaadin/hilla-react-i18n";
+import {translate, key} from "@vaadin/hilla-react-i18n";
+import TitleSearch from "@/components/title-search";
 
 type HeaderProps = {
-  onToggleTheme: () => void;
-  user?: { initials?: string; fullName?: string };
-  onLogout: () => void;
+    onToggleTheme: () => void;
+    user?: { initials?: string; fullName?: string };
+    onLogout: () => void;
 };
 
-export default function Header({ onToggleTheme, user, onLogout }: HeaderProps) {
-  return (
-      <div className="w-full xl:flex xl:justify-center">
-          <HorizontalLayout
-              className="flex justify-between max-w-[1280px] xl:w-[1280px] items-center pe-5 pb-5 pt-1 sm:pe-10 h-22"
-              theme="spacing padding"
-          >
-              <NavLink to="/">
-                  <div className="flex items-center gap-2">
-                      <Logo className="w-[45px]" />
-                      <span className={"text-2xl"}>Hugin</span>
-                  </div>
-              </NavLink>
+export default function Header({onToggleTheme, user, onLogout}: HeaderProps) {
+    const location = useLocation();
+    console.log(location)
+    return (
+        <div className="w-full xl:flex xl:justify-center">
+            <HorizontalLayout
+                className="flex justify-between max-w-[1280px] xl:w-[1280px] items-center pe-5 pb-5 pt-1 sm:pe-10 h-22"
+                theme="spacing padding"
+            >
+                <NavLink to="/">
+                    <div className="flex items-center gap-2">
+                        <Logo className="w-[45px]"/>
+                        <span className={"text-2xl"}>Hugin</span>
+                    </div>
+                </NavLink>
 
-              <div className="flex items-center gap-4">
-                  <HorizontalLayout className="items-center" theme="spacing-xs">
-                      <Avatar>
-                          <AvatarFallback className="dark:text-black text-xs bg-gray-300 header-mono">
-                              {user?.initials}
-                          </AvatarFallback>
-                      </Avatar>
-                      <p className="text-sm !m-0 header-mono hidden sm:inline">{user?.fullName}</p>
-                  </HorizontalLayout>
+                {location.pathname != "/" && <TitleSearch className="p-1"/>}
 
-                  <Button
-                      className="header-mono bg-transparent text-black p-2 min-w-1"
-                      onClick={onLogout}
-                  >
-                      <HorizontalLayout className="items-center text-purple-500 text-sm">
-                          <span className="hidden sm:inline">Logg ut</span>
-                          <LogOut className="ms-1" />
-                      </HorizontalLayout>
-                  </Button>
-              </div>
-          </HorizontalLayout>
-      </div>
+                <div className="flex items-center gap-4">
+                    <HorizontalLayout className="items-center" theme="spacing-xs">
+                        <Avatar>
+                            <AvatarFallback className="dark:text-black text-xs bg-gray-300 header-mono">
+                                {user?.initials}
+                            </AvatarFallback>
+                        </Avatar>
+                        <p className="text-sm !m-0 header-mono hidden sm:inline">{user?.fullName}</p>
+                    </HorizontalLayout>
 
-   );
+                    <Button
+                        className="header-mono bg-transparent text-black p-2 min-w-1"
+                        onClick={onLogout}
+                    >
+                        <HorizontalLayout className="items-center text-purple-500 text-sm">
+                            <span className="hidden sm:inline">Logg ut</span>
+                            <LogOut className="ms-1"/>
+                        </HorizontalLayout>
+                    </Button>
+                </div>
+            </HorizontalLayout>
+        </div>
+
+    );
 }
 
--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -1,21 +1,6 @@
 import {ViewConfig} from "@vaadin/hilla-file-router/types.js";
-import {useAuth} from "@/context/auth-context";
 import Logo from "@/components/logo";
-import React, {useMemo, useRef, useState} from "react";
-import {useCatalogueTitle} from "@/hooks/use-catalogue-title";
-import {
-    Command,
-    CommandEmpty,
-    CommandInput,
-    CommandItem,
-    CommandList,
-} from "@/components/ui/command";
-import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
-import {LoaderCircle} from "lucide-react";
-import {Badge} from "@/components/ui/badge";
-import Title from "@/generated/no/nb/bikube/api/core/model/Title";
-import {isActive} from "@/lib/utils";
-import {clsx} from "clsx";
+import TitleSearch from "@/components/title-search";
 
 export const config: ViewConfig = {
     menu: {
@@ -26,95 +11,11 @@ export const config: ViewConfig = {
 };
 
 export default function MainView() {
-    const {state} = useAuth()
-    const [term, setTerm] = useState("")
-    const [open, setOpen] = useState(false)
-    const contentRef = useRef<HTMLDivElement>(null)
-    const {options, isLoading, selected, setSelected} = useCatalogueTitle({
-        query: term.trim(),
-    });
-
-    const handleSelect = (item: Title) => {
-        setSelected(item as any);
-        // TODO: navigate or do something meaningful:
-    };
-    const rows: Title[] = useMemo(() => options as Title[], [options]);
-
-    // Close when clicking outside the popover content
-    function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
-        // Wait a tick so clicks in the popover can run first
-        setTimeout(() => {
-            const active = document.activeElement as HTMLElement | null
-            if (!contentRef.current?.contains(active ?? null)) {
-                setOpen(false)
-            }
-        }, 0)
-    }
 
     return (
         <div className="flex mt-20 flex-col items-center justify-center">
             <Logo className="w-[150px] mb-5"/>
-            <div className="w-[25rem] max-w-[90vw]">
-                <Command className="w-full" shouldFilter={false}>
-                    <Popover open={open} onOpenChange={setOpen}>
-                        <PopoverTrigger asChild>
-                            <div className=" p-3 border-2 border-gray-200 rounded-full">
-                                <CommandInput
-                                    value={term}
-                                    onValueChange={(v) => {
-                                        setTerm(v)
-                                        if (v.length === 0) setOpen(false)
-                                        if (!open) setOpen(true)
-                                    }}
-                                    onFocus={() => setOpen(true)}
-                                    placeholder="Søk etter avistittel…"
-                                    onKeyDown={(e) => {
-                                        if (e.key === "Escape") setOpen(false)
-                                    }}
-                                />
-                            </div>
-                        </PopoverTrigger>
-
-                        <PopoverContent
-                            ref={contentRef}
-                            onOpenAutoFocus={(e) => e.preventDefault()}
-                            className="my-2 rounded-lg w-[23rem] max-w-[90vw] p-0"
-                        >
-                            <CommandList onMouseDown={(e) => e.preventDefault()}>
-                                {!isLoading && <CommandEmpty>Ingen treff.</CommandEmpty>}
-                                {isLoading &&
-                                    <div className="p-2">
-                                        <LoaderCircle className="animate-spin mx-auto size-10 text-gray-300"/>
-                                    </div>}
-
-                                {rows.map((item: Title) => {
-                                    const active = isActive(item.endDate)
-                                    return (
-                                        <CommandItem
-                                            className={clsx(
-                                              "p-3 my-1 mx-2",
-                                                "data-[selected=true]:bg-gray-300",
-                                                active ? "bg-green-100" : "",
-                                            )}
-                                            value={item.catalogueId}
-                                            key={item.catalogueId}
-                                            onSelect={() => handleSelect(item)}
-                                        >
-                                            {item.name}
-                                            {active &&
-                                                <Badge className="rounded-full bg-green-500 ml-auto">
-                                                    Aktiv
-                                                </Badge>
-                                            }
-                                        </CommandItem>
-                                    )
-                                })}
-
-                            </CommandList>
-                        </PopoverContent>
-                    </Popover>
-                </Command>
-            </div>
+            <TitleSearch className="p-3" />
         </div>
     )
 };
