19a27c16d7bcbe26658cf3575ad595583efe9061
Gard Marius Guldhav <gard.guldhav@nb.no>
2025-11-03 08:07:35 +0100

    Fix/Add Memoryrouter to pass parameters to component

--- src/main/frontend/tests/views/login-test.tsx
+++ src/main/frontend/tests/views/login-test.tsx
@@ -1,17 +1,22 @@
 /// <reference types="vitest" />
-import { describe, it, expect, vi } from 'vitest';
-import { render } from '@testing-library/react';
+import {describe, it, expect, vi} from 'vitest';
+import {render} from '@testing-library/react';
 import Login from '@/views/login';
 
 vi.mock('@/lib/utils', async (orig) => {
     const actual = await orig<typeof import('@/lib/utils')>();
-    return { ...actual, redirect: vi.fn() };
+    return {...actual, redirect: vi.fn()};
 });
-import { redirect } from '@/lib/utils';
+import {redirect} from '@/lib/utils';
+import {MemoryRouter} from "react-router";
 
 describe('Home / login', () => {
     it('Should redirect to hugin root', () => {
-        render(<Login />);
+        render(
+            <MemoryRouter initialEntries={['/?continue=true']}>
+                <Login/>
+            </MemoryRouter>
+        );
         expect(redirect).toHaveBeenCalledWith('/bikube/hugin');
     });
 });
