ba9ad5edfe841b9aa89dca0a2b0e6aa0584b3aa3
Gard Marius Guldhav <gard.guldhav@nb.no>
2025-11-03 13:28:42 +0100

    Fix/Remove unused code

--- src/main/kotlin/no/nb/bikube/hugin/service/HuginCollectionsService.kt
+++ src/main/kotlin/no/nb/bikube/hugin/service/HuginCollectionsService.kt
@@ -2,23 +2,15 @@ package no.nb.bikube.hugin.service
 
 import com.vaadin.hilla.BrowserCallable
 import jakarta.annotation.security.RolesAllowed
-import kotlinx.coroutines.awaitAll
 import no.nb.bikube.api.core.model.Title
 import no.nb.bikube.api.newspaper.service.NewspaperService
 import no.nb.bikube.api.newspaper.service.TitleIndexService
-import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Mono
 
 @BrowserCallable
 class HuginCollectionsService(
     private val titleIndexService: TitleIndexService,
     private val newspaperService: NewspaperService,
 ) {
-
-    private val webClient = WebClient
-        .builder()
-        .baseUrl("/")
-        .build()
 
     @RolesAllowed("T_dimo_admin", "T_dimo_user")
     fun findByTitle(searchTerm: String): List<Title> {
