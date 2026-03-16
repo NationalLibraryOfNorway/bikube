7681222e8b132dad7556a1859fdb4515a34a7a91
Sindre Østrem <sindre.ostrem@nb.no>
2026-03-16 15:00:51 +0100

    Increase timout to collections to 20 minutes

--- src/main/kotlin/no/nb/bikube/api/catalogue/collections/config/CollectionsWebClient.kt
+++ src/main/kotlin/no/nb/bikube/api/catalogue/collections/config/CollectionsWebClient.kt
@@ -1,25 +1,16 @@
 package no.nb.bikube.api.catalogue.collections.config
 
-import io.netty.handler.timeout.TimeoutException
-import no.nb.bikube.api.core.util.logger
-import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.*
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClient.Builder
-import org.springframework.web.reactive.function.client.WebClientRequestException
 import reactor.netty.http.client.HttpClient
-import reactor.util.retry.Retry
-import reactor.util.retry.RetryBackoffSpec
 import java.time.Duration
 
 @Profile("!test")
@@ -53,7 +44,7 @@ class CollectionsWebClientConfig(
     @Bean
     fun collectionsWebClient(): WebClient {
         val httpClient = HttpClient.create()
-            .responseTimeout(Duration.ofSeconds(30))
+            .responseTimeout(Duration.ofMinutes(2))
 
         val oauth = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
         oauth.setDefaultClientRegistrationId("keycloak-collections") // Uses the 'keycloak-collections' client registration as configured in application properties
