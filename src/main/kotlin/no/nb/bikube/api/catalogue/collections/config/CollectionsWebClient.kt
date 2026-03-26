6ee2f5d81995e0aacebcbbe72cf3928413779d93
Sindre Østrem <sindre.ostrem@nb.no>
2026-03-26 09:09:12 +0100

    Feat: Set retryStrategy against collections (TT-2246) (#131)

--- src/main/kotlin/no/nb/bikube/api/catalogue/collections/config/CollectionsWebClient.kt
+++ src/main/kotlin/no/nb/bikube/api/catalogue/collections/config/CollectionsWebClient.kt
@@ -7,10 +7,15 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.security.oauth2.client.*
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClient.Builder
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.core.publisher.Mono
 import reactor.netty.http.client.HttpClient
+import reactor.util.retry.Retry
+import java.io.IOException
 import java.time.Duration
 
 @Profile("!test")
@@ -49,10 +54,25 @@ class CollectionsWebClientConfig(
         val oauth = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
         oauth.setDefaultClientRegistrationId("keycloak-collections") // Uses the 'keycloak-collections' client registration as configured in application properties
 
+        val retryStrategy = Retry.backoff(30, Duration.ofMillis(500))
+            .maxBackoff(Duration.ofSeconds(5))
+            .filter { it is IOException || it is WebClientResponseException.ServiceUnavailable }
+
+        val errorOnServerError = ExchangeFilterFunction.ofResponseProcessor { response ->
+            if (response.statusCode().is5xxServerError) response.createError()
+            else Mono.just(response)
+        }
+
+        val retryFilter = ExchangeFilterFunction { request, next ->
+            next.exchange(request).retryWhen(retryStrategy)
+        }
+
         val webClientBuilder: Builder = WebClient.builder()
             .clientConnector(ReactorClientHttpConnector(httpClient))
             .baseUrl(collectionsConfig.url)
             .filter(oauth)
+            .filter(errorOnServerError)
+            .filter(retryFilter)
             .exchangeStrategies(
                 ExchangeStrategies.builder().codecs { configurer ->
                     configurer.defaultCodecs().maxInMemorySize(64 * 1024 * 1024)
